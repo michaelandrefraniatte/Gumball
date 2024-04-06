4e7fdeab0d8aa9c0bebef236646fce52b2d6ea26
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-04-06 22:37:32 +0200

    Initial commit

---
+++ Src/Gumball/Gumball/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Gumball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbp1 = new System.Windows.Forms.Label();
+            this.lbp2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(442, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gumball";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(459, 638);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Player 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(459, 676);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Player 2";
+            // 
+            // lbp1
+            // 
+            this.lbp1.AutoSize = true;
+            this.lbp1.BackColor = System.Drawing.Color.Transparent;
+            this.lbp1.Location = new System.Drawing.Point(431, 638);
+            this.lbp1.Name = "lbp1";
+            this.lbp1.Size = new System.Drawing.Size(14, 16);
+            this.lbp1.TabIndex = 3;
+            this.lbp1.Text = ">";
+            // 
+            // lbp2
+            // 
+            this.lbp2.AutoSize = true;
+            this.lbp2.BackColor = System.Drawing.Color.Transparent;
+            this.lbp2.Location = new System.Drawing.Point(431, 676);
+            this.lbp2.Name = "lbp2";
+            this.lbp2.Size = new System.Drawing.Size(0, 16);
+            this.lbp2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(528, 635);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(529, 673);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Gumball.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(982, 753);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbp2);
+            this.Controls.Add(this.lbp1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Gumball";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbp1;
+        private System.Windows.Forms.Label lbp2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Src/Gumball/Gumball/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gumball
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Region rg;
+        private GraphicsPath gp;
+        public List<PictureBox> pictureboxes = new List<PictureBox>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gp = new GraphicsPath();
+            gp.AddEllipse(pictureBox1.DisplayRectangle);
+            rg = new Region(gp);
+            pictureBox1.Region = rg;
+            pictureBox2.Region = rg;
+            for (int i = 0; i < 64; i++)
+            {
+                pictureboxes.Add(new PictureBox());
+            }
+            int lx = 0;
+            int ly = 0;
+            foreach (PictureBox picturebox in pictureboxes)
+            {
+                picturebox.Cursor = Cursors.Hand;
+                picturebox.Size = new System.Drawing.Size(35, 35);
+                picturebox.BackColor = System.Drawing.Color.Silver;
+                picturebox.Location = new Point(195 + 10 * lx + picturebox.Size.Width * lx, 115 + 10 * ly + picturebox.Size.Height * ly);
+                gp = new GraphicsPath();
+                gp.AddEllipse(picturebox.DisplayRectangle);
+                rg = new Region(gp);
+                picturebox.Region = rg;
+                lx++;
+                if (lx > 7)
+                {
+                    lx = 0;
+                    ly++;
+                }
+                this.Controls.Add(picturebox);
+                picturebox.BringToFront();
+            }
+            AddEvents();
+        }
+        private void AddEvents()
+        {
+            foreach (var (picturebox, index) in pictureboxes.WithIndex())
+            {
+                picturebox.Click += (sender, e) =>
+                {
+                    ProcessLogic(index);
+                };
+            }
+        }
+        private void ProcessLogic(int index)
+        {
+            if (pictureboxes[index].BackColor == Color.Silver)
+            {
+                bool player1turn = false;
+                bool player2turn = false;
+                if (lbp1.Text == ">")
+                {
+                    pictureboxes[index].BackColor = Color.White;
+                    if (index > 7 & index < 56)
+                    {
+                        if (pictureboxes[index - 8].BackColor == Color.White & pictureboxes[index + 8].BackColor == Color.White)
+                        {
+                            player1turn = true;
+                        }
+                    }
+                    if (!player1turn)
+                    {
+                        if ((index > 0 & index < 7) | (index > 8 & index < 15) | (index > 16 & index < 23) | (index > 24 & index < 31) | (index > 32 & index < 39) | (index > 40 & index < 47) | (index > 48 & index < 55) | (index > 56 & index < 63))
+                        {
+                            if (pictureboxes[index - 1].BackColor == Color.White & pictureboxes[index + 1].BackColor == Color.White)
+                            {
+                                player1turn = true;
+                            }
+                        }
+                    }
+                    if (!player1turn)
+                    {
+                        lbp1.Text = "";
+                        lbp2.Text = ">";
+                    }
+                }
+                else if (lbp2.Text == ">")
+                {
+                    pictureboxes[index].BackColor = Color.Black;
+                    if (index > 7 & index < 56)
+                    {
+                        if (pictureboxes[index - 8].BackColor == Color.Black & pictureboxes[index + 8].BackColor == Color.Black)
+                        {
+                            player2turn = true;
+                        }
+                    }
+                    if (!player2turn)
+                    {
+                        if ((index > 0 & index < 7) | (index > 8 & index < 15) | (index > 16 & index < 23) | (index > 24 & index < 31) | (index > 32 & index < 39) | (index > 40 & index < 47) | (index > 48 & index < 55) | (index > 56 & index < 63))
+                        {
+                            if (pictureboxes[index - 1].BackColor == Color.Black & pictureboxes[index + 1].BackColor == Color.Black)
+                            {
+                                player2turn = true;
+                            }
+                        }
+                    }
+                    if (!player2turn)
+                    {
+                        lbp1.Text = ">";
+                        lbp2.Text = "";
+                    }
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbp1.Text = ">";
+            lbp2.Text = "";
+            foreach (PictureBox picturebox in pictureboxes)
+            {
+                picturebox.BackColor = Color.Silver;
+            }
+        }
+    }
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> self) => self.Select((item, index) => (item, index));
+    }
+}
