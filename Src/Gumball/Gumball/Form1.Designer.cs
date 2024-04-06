67a78b812f1c8deffdc7338b6ac22f6a43286824
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-04-07 01:19:10 +0200

    Add about

--- Src/Gumball/Gumball/Form1.Designer.cs
+++ Src/Gumball/Gumball/Form1.Designer.cs
@@ -151,6 +151,7 @@
             this.Name = "Form1";
             this.Text = "Gumball";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
